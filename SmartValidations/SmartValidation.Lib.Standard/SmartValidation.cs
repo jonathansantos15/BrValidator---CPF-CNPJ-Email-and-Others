a2a8c7f3b654816938a1911acebf7c92e98cc962
Jonathan Santos <jonathan.santos15a@hotmail.com>
2018-09-07 14:27:20 -0300

    Creating Solution and First Tests
    Solution Created with CPF tests and SmarValidation lib.

---
+++ SmartValidations/SmartValidation.Lib.Standard/SmartValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SmartValidation.Lib.Standard
+{
+    public static class SmartValidation
+    {
+        public static void ValidateMail(String mail)
+        {
+
+        }
+
+        public static Boolean ValidateCPF(String cpf)
+        {
+            if (cpf.Length < 11 || cpf.Length > 11)
+                return false;
+
+            if (cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444"
+                || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888"
+                || cpf == "99999999999" || cpf == "00000000000")
+                return false;
+
+            int sumChars = 0;
+
+            for (int i = 0, j = 11; i < cpf.Length-1; i++, j--)
+                sumChars += Convert.ToInt32(cpf[i].ToString()) * j;
+
+            sumChars = (sumChars * 10) % 11;
+
+            return sumChars.ToString() == cpf[10].ToString() ? true : false;
+        }
+    }
+}
